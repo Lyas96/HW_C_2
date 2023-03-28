f1e75931da66515084ac245ef12fc7aa2436a4b0
Lyas <Egorlyasotsky@yandex.ru>
2023-03-28 18:41:43 +0400

    решена задача номер 10

--- task_10/Program.cs
+++ task_10/Program.cs
@@ -1,1 +1,7 @@
 ﻿// Программа принимает трехзначное число и показывает вторую цифру числа.
+Console.Clear();
+int a = new Random().Next(100, 1000);
+Console.WriteLine($"{a}");
+int b = a / 10;
+int c = b % 10;
+Console.WriteLine($"{c}");
