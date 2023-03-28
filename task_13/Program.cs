4d24cbc18115a8d90d96a76773e1e8f5fb637b09
Lyas <Egorlyasotsky@yandex.ru>
2023-03-28 20:04:12 +0400

    решена задача номер 13

--- task_13/Program.cs
+++ task_13/Program.cs
@@ -1,1 +1,20 @@
 ﻿// Программа выводит третью цифру заданного числа, или сообщает что третьей цифры нет.
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+while (num / 1000 != 0)
+{
+  num = num / 10;
+}
+if ((num > 99) || (num < -99))
+{
+  int num1 = num % 10;
+  if(num1 < 0) num1 = num1 * -1;
+  Console.WriteLine($"Третья цифра числа: {num1}");
+  }
+else
+{
+Console.WriteLine("Третьей цифры нет");
+}
+
